51b148b0ce23df3abf467e4b7077a0e62b74a68d
sffxzzp <sffxzzp@126.com>
2018-05-10 19:07:38 +0800

    给视频添加模式切换

--- view/material/show/video.php
+++ view/material/show/video.php
@@ -2,6 +2,7 @@
 <?php $mpd =  str_replace("thumbnail","videomanifest",$item['thumb'])."&part=index&format=dash&useScf=True&pretranscode=0&transcodeahead=0";?>
 <?php view::begin('content');?>
 <link class="dplayer-css" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/dplayer/dist/DPlayer.min.css">
+<script src="https://cdn.jsdelivr.net/npm/dashjs/dist/dash.all.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/dplayer/dist/DPlayer.min.js"></script>
 <script src="view/js/subtitles-octopus.js"></script>
 <div class="mdui-container-fluid">
@@ -19,30 +20,65 @@
 	</div>
 </div>
 <script>
-const dp = new DPlayer({
-	container: document.getElementById('dplayer'),
-	lang:'zh-cn',
-	video: {
-	    url: '<?php e($item['downloadUrl']);?>',
-	    pic: '<?php @e($item['thumb'].'&width=176&height=176');?>',
-	    type: 'auto'
-	}
-});
-dp.on('canplay', function () {
-    var video = document.getElementsByTagName('video')[0];
-    window.SubtitlesOctopusOnLoad = function () {
-        var options = {
-            video: video,
-            subUrl: '<?php $urlparts = pathinfo($url); e($urlparts['dirname'].'/'.$urlparts['filename'].'.ass');?>',
-            fonts: ["//gapis.geekzu.org/g-fonts/ea/notosanssc/v1/NotoSansSC-Regular.otf", "//gapis.geekzu.org/g-fonts/ea/notosanstc/v1/NotoSansTC-Regular.otf", "//gapis.geekzu.org/g-fonts/ea/notosansjapanese/v6/NotoSansJP-Regular.otf"],
-            workerUrl: 'view/js/subtitles-octopus-worker.js'
-        };
-        window.octopusInstance = new SubtitlesOctopus(options);
-    };
-    if (SubtitlesOctopus) {
-        SubtitlesOctopusOnLoad();
+var dp;
+function loadPlayer(mode) {
+    if (mode==0) {
+        dp = new DPlayer({
+            container: document.getElementById('dplayer'),
+            lang:'zh-cn',
+            video: {
+                url: '<?php e($item['downloadUrl']);?>',
+                pic: '<?php @e($item['thumb'].'&width=176&height=176');?>',
+                type: 'auto'
+            }
+        });
     }
-});
+    else {
+        dp = new DPlayer({
+            container: document.getElementById('dplayer'),
+            lang:'zh-cn',
+            video: {
+                url: '<?php echo $mpd;?>',
+                pic: '<?php @e($item['thumb'].'&width=176&height=176');?>',
+                type: 'dash'
+            }
+        });
+    }
+}
+function subtitle() {
+    dp.on('canplay', function () {
+        var video = document.getElementsByTagName('video')[0];
+        window.SubtitlesOctopusOnLoad = function () {
+            var options = {
+                video: video,
+                subUrl: '<?php $urlparts = pathinfo($url); e($urlparts['dirname'].'/'.$urlparts['filename'].'.ass');?>',
+                fonts: ["//gapis.geekzu.org/g-fonts/ea/notosanssc/v1/NotoSansSC-Regular.otf", "//gapis.geekzu.org/g-fonts/ea/notosanstc/v1/NotoSansTC-Regular.otf", "//gapis.geekzu.org/g-fonts/ea/notosansjapanese/v6/NotoSansJP-Regular.otf"],
+                workerUrl: 'view/js/subtitles-octopus-worker.js'
+            };
+            window.octopusInstance = new SubtitlesOctopus(options);
+        };
+        if (SubtitlesOctopus) {
+            SubtitlesOctopusOnLoad();
+        }
+    });
+}
+function modeSwitch() {
+    dp.destroy();
+    loadPlayer(1);
+    subtitle();
+};
+loadPlayer(0);
+subtitle();
 </script>
-<a href="<?php e($url);?>" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
+
+<div class="mdui-fab-wrapper" mdui-fab="{trigger: 'click'}">
+    <button class="mdui-fab mdui-ripple mdui-color-theme-accent">
+        <i class="mdui-icon material-icons">settings</i>
+        <i class="mdui-icon mdui-fab-opened material-icons">settings</i>
+    </button>
+    <div class="mdui-fab-dial">
+        <a href="<?php e($url);?>" class="mdui-fab mdui-fab-mini mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
+        <a onclick="javascript:modeSwitch();" class="mdui-fab mdui-fab-mini mdui-ripple mdui-color-red"><i class="mdui-icon material-icons">sync_problem</i></a>
+    </div>
+</div>
 <?php view::end('content');?>
